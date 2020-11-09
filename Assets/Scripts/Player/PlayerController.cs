930363ef710a04d7af352a802d3a2a3a4b6c1d76
Max Himmel <himmelmax@gmail.com>
2020-11-09 10:32:05 -0800

    + reworked movement AGAIN (the best yet?!) -- probably not!

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -14,28 +14,36 @@ namespace Moonshot.Gameplay.Player
 		[Header( "Orbit" )]
 		[SerializeField] private Rigidbody2D m_orbitTarget = default;
 		[SerializeField] private float m_gravityForce = 2;
+		[SerializeField] private float m_groundOffset = 0.1f;
 
 		[Header( "Movement" )]
+		[SerializeField] private bool m_beginGrounded = true;
 		[SerializeField] private float m_acceleration = 10;
 		[SerializeField] private float m_maxSpeed = 5;
 		[SerializeField] private float m_jumpHeight = 3;
-		[SerializeField] private float m_groundedProbeLength = 0.2f;
-		[SerializeField] private LayerMask m_groundedProbeLayer = 0;
+
+		[Header( "Pushing" )]
+		[SerializeField] private float m_jumpPushForce = 10;
+		[SerializeField] private float m_landPushForce = 10;
 
 		private Rewired.Player m_input = null;
 
 		private Rigidbody2D m_rigidbody = null;
 		private CircleCollider2D m_collider = null;
 		
-		private Vector3 m_velocity = Vector3.zero;
-		private Vector3 m_desiredVelocity = Vector3.zero;
 		private Vector3 m_gravityNormal = Vector3.up;
-		private bool m_isGrounded = false;
+		private float m_currentAngle = 0;
+		private float m_currentSpeed = 0;
+		private float m_currentGravity = 0;
+		private float m_currentJumpForce = 0;
+		private Vector3 m_desiredMoveDirection = Vector3.zero;
 		private bool m_isJumpDesired = false;
+		private bool m_isGrounded = false;
 
 		private void Update()
 		{
-			SetDesiredVelocity( GetMoveInput() );
+			Vector3 moveInput = GetMoveInput();
+			SetMoveDirection( moveInput );
 
 			if ( m_input.GetButtonDown( Action.Jump ) )
 			{
@@ -43,9 +51,18 @@ namespace Moonshot.Gameplay.Player
 			}
 		}
 
-		private void SetDesiredVelocity( Vector3 moveDirection )
+		private Vector3 GetMoveInput()
 		{
-			m_desiredVelocity = moveDirection * m_maxSpeed;
+			Vector2 rawMoveInput = m_input.GetAxis2D( Action.MoveHorizontal, Action.MoveVertical );
+			Vector2 clampedMoveInput = Vector2.ClampMagnitude( rawMoveInput, 1 );
+			Vector3 moveInput = Vector3.ProjectOnPlane( clampedMoveInput, m_gravityNormal );
+
+			return moveInput;
+		}
+
+		private void SetMoveDirection( Vector3 moveDirection )
+		{
+			m_desiredMoveDirection = moveDirection;
 		}
 
 		private bool TryJump()
@@ -56,79 +73,121 @@ namespace Moonshot.Gameplay.Player
 			return true;
 		}
 
-		private Vector3 GetMoveInput()
-		{
-			Vector2 rawMoveInput = m_input.GetAxis2D( Action.MoveHorizontal, Action.MoveVertical );
-			Vector2 clampedMoveInput = Vector2.ClampMagnitude( rawMoveInput, 1 );
-			Vector3 moveInput = Vector3.ProjectOnPlane( clampedMoveInput, m_gravityNormal ).normalized * clampedMoveInput.magnitude;
-
-			return moveInput;
-		}
-
 		private void FixedUpdate()
 		{
 			UpdateState();
 
-			Accelerate();	
-			
+			Accelerate();
+			ApplyAngleVelocity();
+
 			if ( m_isJumpDesired )
 			{
 				Jump();
 			}
 
 			ApplyGravity();
-			ApplyVelocity();
+			ApplyFinalMovement();
 		}
 
 		private void UpdateState()
 		{
-			m_velocity = m_rigidbody.velocity;
-			m_gravityNormal = GetGravityNormal();
-			m_isGrounded = Physics2D.Raycast( m_rigidbody.position, -GetGravityNormal(), m_groundedProbeLength, m_groundedProbeLayer );
+			m_gravityNormal = Quaternion.Euler( 0, 0, m_currentAngle ) * Vector2.up;
+
+			bool wasPreviouslyGrounded = m_isGrounded;
+			m_isGrounded = m_currentGravity <= 0;
+
+			if ( !wasPreviouslyGrounded && m_isGrounded )
+			{
+				OnLanded();
+			}
 		}
 
-		private Vector3 GetGravityNormal()
+		private void OnLanded()
 		{
-			return (m_rigidbody.position - m_orbitTarget.position).normalized;
+			PushOrbitTarget( -m_gravityNormal * m_landPushForce );
 		}
 
 		private void Accelerate()
 		{
-			Vector3 xAxis = Vector3.ProjectOnPlane( Vector3.right, m_gravityNormal ).normalized;
-			Vector3 yAxis = Vector3.ProjectOnPlane( Vector3.up, m_gravityNormal ).normalized;
-
-			float currentX = Vector3.Dot( m_velocity, xAxis );
-			float currentY = Vector3.Dot( m_velocity, yAxis );
-
+			float targetAngle = Vector2.SignedAngle( Vector2.up, m_desiredMoveDirection );
+			int moveDir = System.Math.Sign( Mathf.DeltaAngle( m_currentAngle, targetAngle ) );
+			
 			float speedDelta = m_acceleration * Time.deltaTime;
-			float newX = Mathf.MoveTowards( currentX, m_desiredVelocity.x, speedDelta );
-			float newY = Mathf.MoveTowards( currentY, m_desiredVelocity.y, speedDelta );
-
-			m_velocity += xAxis * (newX - currentX) + yAxis * (newY - currentY);
+			m_currentSpeed = Mathf.MoveTowards( m_currentSpeed, m_desiredMoveDirection.magnitude * m_maxSpeed * moveDir, speedDelta );
 		}
 
-		private void ApplyGravity()
+		private void ApplyAngleVelocity()
 		{
-			float gravForce = m_gravityForce * m_rigidbody.gravityScale;
-			m_velocity -= m_gravityNormal * gravForce * Time.deltaTime;
-		}
-
-		private void ApplyVelocity()
-		{
-			m_rigidbody.velocity = m_velocity;
+			m_currentAngle += m_currentSpeed;
 		}
 
 		private void Jump()
 		{
 			m_isJumpDesired = false;
 
-			float jumpForce = Mathf.Sqrt( 2f * m_gravityForce * m_jumpHeight );
-			m_velocity += GetGravityNormal() * jumpForce;
+			float gravForce = m_gravityForce * m_rigidbody.gravityScale;
+			m_currentJumpForce = Mathf.Sqrt( 2.0f * gravForce * m_jumpHeight );
+
+			PushOrbitTarget( -m_gravityNormal * m_jumpPushForce );
+		}
+
+		private void ApplyGravity()
+		{
+			float gravDelta = m_gravityForce * m_rigidbody.gravityScale * Time.deltaTime;
+
+			m_currentGravity += m_currentJumpForce * Time.deltaTime;
+			m_currentJumpForce -= gravDelta;
+
+			m_currentGravity -= gravDelta * Time.deltaTime;
+			m_currentGravity = Mathf.Max( 0, m_currentGravity );
+		}
+
+		private void ApplyFinalMovement()
+		{
+			m_rigidbody.MovePosition( GetOrbitPlacement() );
+		}
+
+		private Vector2 GetOrbitPlacement()
+		{
+			float distFromOrbitCenter = GetSurfaceRadius() + GetPlayerRadius() + m_groundOffset + m_currentGravity;
+
+			const float polarCoordOffset = 90;
+			float angle2Rad = (m_currentAngle + polarCoordOffset) * Mathf.Deg2Rad;
+
+			Vector2 dirFromOrbit = new Vector2()
+			{
+				x = distFromOrbitCenter * Mathf.Cos( angle2Rad ),
+				y = distFromOrbitCenter * Mathf.Sin( angle2Rad )
+			};
+
+			return m_orbitTarget.position + dirFromOrbit;
+		}
+
+		private float GetSurfaceRadius()
+		{
+			CircleCollider2D orbitCollider = m_orbitTarget.GetComponentInChildren<CircleCollider2D>();
+			return orbitCollider.radius;
+		}
+
+		private float GetPlayerRadius()
+		{
+			return m_collider.radius;
+		}
+
+		private void PushOrbitTarget( Vector3 force )
+		{
+			m_orbitTarget.AddForce( force, ForceMode2D.Impulse );
 		}
 
 		private void Start()
 		{
 			m_input = ReInput.players.GetPlayer( 0 );
+
+			if ( m_beginGrounded )
+			{
+				ApplyFinalMovement();
+				m_isGrounded = true;
+			}
 		}
 
 		private void Awake()
