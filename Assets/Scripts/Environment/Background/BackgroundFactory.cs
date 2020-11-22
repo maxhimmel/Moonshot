aa16ccff89717501c03d387772d8887b4d51ce43
Max Himmel <himmelmax@gmail.com>
2020-11-22 14:26:27 -0800

    + Added background layer for gameobject rendering and camera culling

--- Assets/Scripts/Environment/Background/BackgroundFactory.cs
+++ Assets/Scripts/Environment/Background/BackgroundFactory.cs
@@ -7,18 +7,30 @@ namespace Moonshot.Gameplay.Environment.Factory
 {
 	public class BackgroundFactory : Factory<Background>
 	{
-		protected override void Awake()
-		{
-			m_prefab.Init();
-
-			base.Awake();
-		}
-
 		public Vector2 GetLossyTileSize()
 		{
 			if ( m_prefab == null ) { return Vector2.zero; }
 
 			return m_prefab.GetLossyTileSize();
+		}
+
+		public override Background Create( Vector3 position = default, Quaternion rotation = default, Transform parent = null )
+		{
+			Background newBg = base.Create( position, rotation, parent );
+
+			if ( newBg != null )
+			{
+				newBg.gameObject.layer = gameObject.layer;
+			}
+
+			return newBg;
+		}
+
+		protected override void Awake()
+		{
+			m_prefab.Init();
+
+			base.Awake();
 		}
 	}
 }
