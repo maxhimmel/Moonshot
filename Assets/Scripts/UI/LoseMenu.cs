68553f0adf79b5d57e532d406447bd4d93dbb2f9
Max Himmel <himmelmax@gmail.com>
2020-11-28 12:33:28 -0800

    + Added win/lose menu to level kit and hooked into gamemode

---
+++ Assets/Scripts/UI/LoseMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Xam;
+
+namespace Moonshot.Gameplay.UI
+{
+	public class LoseMenu : MonoBehaviour
+	{
+		[SerializeField] private Button m_againButton = default;
+		[SerializeField] private Button m_exitButton = default;
+
+		private CanvasGroup m_canvasGroup = null;
+
+		private void Awake()
+		{
+			m_canvasGroup = GetComponent<CanvasGroup>();
+		}
+
+		private void Start()
+		{
+			m_againButton.onClick.AddListener( GameManager.Instance.ReloadActiveLevel );
+			m_exitButton.onClick.AddListener( GameManager.Instance.LoadMainMenu );
+
+			GameMode.Instance.OnGameLostEvent += OnGameLost;
+
+			gameObject.SetActive( false );
+		}
+
+		private void OnGameLost( GameMode gameMode )
+		{
+			gameObject.SetActive( true );
+			m_canvasGroup.alpha = 1;
+		}
+	}
+}
---
+++ Assets/Scripts/UI/WinMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Xam;
+
+namespace Moonshot.Gameplay.UI
+{
+	public class WinMenu : MonoBehaviour
+	{
+		[SerializeField] private Button m_againButton = default;
+		[SerializeField] private Button m_exitButton = default;
+
+		private CanvasGroup m_canvasGroup = null;
+
+		private void Awake()
+		{
+			m_canvasGroup = GetComponent<CanvasGroup>();
+		}
+
+		private void Start()
+		{
+			m_againButton.onClick.AddListener( GameManager.Instance.ReloadActiveLevel );
+			m_exitButton.onClick.AddListener( GameManager.Instance.LoadMainMenu );
+
+			GameMode.Instance.OnGameWonEvent += OnGameWon;
+
+			gameObject.SetActive( false );
+		}
+
+		private void OnGameWon( GameMode gameMode )
+		{
+			gameObject.SetActive( true );
+			m_canvasGroup.alpha = 1;
+		}
+	}
+}
