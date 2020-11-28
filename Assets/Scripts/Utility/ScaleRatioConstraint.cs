42c94b283f500aefb930beb77af178c85677fb1a
Max Himmel <himmelmax@gmail.com>
2020-11-27 20:29:57 -0800

    + Fixed typo with namespace

--- Assets/Scripts/Utility/ScaleRatioConstraint.cs
+++ Assets/Scripts/Utility/ScaleRatioConstraint.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Moonshot.Utlity
+namespace Moonshot.Utility
 {
 	[ExecuteInEditMode]
 	public class ScaleRatioConstraint : MonoBehaviour
