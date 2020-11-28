bdc456cfded580df844b27dd2382f626786d7225
Max Himmel <himmelmax@gmail.com>
2020-11-27 20:32:04 -0800

    + Created vortex prefab (purely visual for now) + Added vortex prefab to sample scene

---
+++ Assets/Scripts/Environment/Vortex.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Xam.Gameplay;
+
+namespace Moonshot.Gameplay
+{
+	using Player;
+
+	public class Vortex : ProximityBucket2D<PlayerController>
+	{
+		protected override void OnTargetEntered( PlayerController target )
+		{
+			Debug.Log( $"PLAYER ENTERED ZONE - <size=20>WIN GAME</size>" );
+
+			base.OnTargetEntered( target );
+		}
+	}
+}
