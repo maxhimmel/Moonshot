c4ffeec8902d7f4ac098d83e91481384f1eb9bf4
Max Himmel <himmelmax@gmail.com>
2020-11-27 20:31:32 -0800

    + Created animated scaler and rotator

---
+++ Assets/Scripts/Utility/RotateFx.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Xam.Utility.Extensions;
+
+namespace Moonshot.Utility
+{
+	public class RotateFx : MonoBehaviour
+	{
+		public bool IsPlaying { get { return m_updateRoutine != null; } }
+
+		[SerializeField] private bool m_playOnStart = true;
+		[SerializeField] private bool m_isLooping = true;
+
+		[Space]
+		[SerializeField] private Vector3 m_axis = Vector3.forward;
+		[SerializeField] private AnimationCurve m_animCurve = AnimationCurve.Linear( 0, 0, 1, 1 );
+
+		private Coroutine m_updateRoutine = null;
+
+		private void Start()
+		{
+			if ( m_playOnStart )
+			{
+				Play();
+			}
+		}
+
+		public void Stop()
+		{
+			if ( IsPlaying )
+			{
+				StopCoroutine( m_updateRoutine );
+				m_updateRoutine = null;
+			}
+		}
+
+		public void Play()
+		{
+			if ( IsPlaying ) { return; }
+
+			m_updateRoutine = StartCoroutine( Update_Coroutine() );
+		}
+
+		private IEnumerator Update_Coroutine()
+		{
+			float duration = m_animCurve.GetDuration();
+			float timer = 0;
+
+			while ( timer < duration || m_isLooping )
+			{
+				timer += Time.deltaTime;
+
+				float value = m_animCurve.Evaluate( timer );
+				transform.localRotation = Quaternion.Euler( m_axis * value );
+
+				yield return null;
+			}
+
+			m_updateRoutine = null;
+		}
+	}
+}
