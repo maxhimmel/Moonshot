29bf63ba5109fe584e87750c3fbb3c24a901a364
Max Himmel <himmelmax@gmail.com>
2020-11-27 21:33:22 -0800

    + Created select marker helper and button prefab

---
+++ Assets/Scripts/UI/SelectMarkerElement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Moonshot.Gameplay.UI
+{
+	public class SelectMarkerElement : MonoBehaviour
+	{
+		private CanvasGroup m_canvasGroup = null;
+
+		private void Awake()
+		{
+			m_canvasGroup = GetComponent<CanvasGroup>();
+			Hide( null );
+
+			Transform parent = transform.parent;
+			Debug.Assert( parent != null, $"SelectMarkerElement requires a parent!", this );
+
+			EventTrigger parentTrigger = parent.gameObject.AddComponent<EventTrigger>();
+
+			//parentTrigger.triggers.Add( CreateEvent( EventTriggerType.PointerEnter, Show ) );
+			//parentTrigger.triggers.Add( CreateEvent( EventTriggerType.PointerExit, Hide ) );
+
+			parentTrigger.triggers.Add( CreateEvent( EventTriggerType.Select, Show ) );
+			parentTrigger.triggers.Add( CreateEvent( EventTriggerType.Deselect, Hide ) );
+		}
+
+		private EventTrigger.Entry CreateEvent( EventTriggerType type, UnityAction<BaseEventData> @event )
+		{
+			EventTrigger.Entry newEvent = new EventTrigger.Entry();
+			newEvent.eventID = type;
+			newEvent.callback.AddListener( @event );
+
+			return newEvent;
+		}
+
+		private void Show( BaseEventData data )
+		{
+			m_canvasGroup.alpha = 1;
+		}
+
+		private void Hide( BaseEventData data )
+		{
+			m_canvasGroup.alpha = 0;
+		}
+	}
+}
