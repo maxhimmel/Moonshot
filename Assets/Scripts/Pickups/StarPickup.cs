ca617abcf42882d813309265937db19737a5dc9e
Max Himmel <himmelmax@gmail.com>
2020-11-29 15:55:36 -0800

    + Stars now scale down once collected

--- Assets/Scripts/Pickups/StarPickup.cs
+++ Assets/Scripts/Pickups/StarPickup.cs
@@ -7,8 +7,10 @@ namespace Moonshot.Gameplay.Pickups
 	public class StarPickup : MonoBehaviour, IPickup
 	{
 		[SerializeField] private float m_energyRefill = 10;
+		[SerializeField] private float m_deathDuration = 0.65f;
 
 		private Rigidbody2D m_rigidbody = null;
+		private Coroutine m_deathRoutine = null;
 
 		void IPickup.Acquire()
 		{
@@ -17,6 +19,30 @@ namespace Moonshot.Gameplay.Pickups
 				EnergyManager.Instance.AddEnergy( m_energyRefill );
 			}
 
+			if ( m_deathRoutine == null )
+			{
+				m_deathRoutine = StartCoroutine( UpdateDeath_Coroutine() );
+			}
+		}
+
+		private IEnumerator UpdateDeath_Coroutine()
+		{
+			Vector3 startScale = transform.localScale;
+			Vector3 endScale = Vector3.zero;
+
+			float timer = 0;
+			while ( timer < 1 )
+			{
+				timer += Time.deltaTime / m_deathDuration;
+				timer = Mathf.Min( timer, 1 );
+
+				Vector3 newScale = Vector3.Lerp( startScale, endScale, timer );
+				transform.localScale = newScale;
+
+				yield return null;
+			}
+
+			m_deathRoutine = null;
 			Destroy( gameObject );
 		}
 
