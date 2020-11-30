6774d2573340eaf7c1b1d307de6dfabfbb42f99b
Max Himmel <himmelmax@gmail.com>
2020-11-29 16:07:30 -0800

    + Added planet bounce when collisions occur!

---
+++ Assets/Scripts/Environment/Planet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Xam.Utility.Juicy;
+
+namespace Moonshot.Gameplay.Environment
+{
+	public class Planet : MonoBehaviour
+	{
+		[SerializeField] private float m_bounceDuration = 0.5f;
+
+		private BounceFx m_bounceFx = null;
+
+		private void OnCollisionEnter2D( Collision2D collision )
+		{
+			m_bounceFx.Play( m_bounceDuration );
+		}
+
+		private void Awake()
+		{
+			m_bounceFx = GetComponent<BounceFx>();
+		}
+	}
+}
